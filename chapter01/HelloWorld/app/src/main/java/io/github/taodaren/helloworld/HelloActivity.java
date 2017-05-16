0b162693448b28296af52312ed5a60887e2e1674
taodaren <taodaren1125@gmail.com>
2017-05-17 06:42:25 +0800

    Log日志工具使用

--- chapter01/HelloWorld/app/src/main/java/io/github/taodaren/helloworld/HelloActivity.java
+++ chapter01/HelloWorld/app/src/main/java/io/github/taodaren/helloworld/HelloActivity.java
@@ -2,12 +2,18 @@ package io.github.taodaren.helloworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HelloActivity extends AppCompatActivity {
+    //输入「logt」快速生成常量 TAG
+    private static final String TAG = "HelloActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
+        //日志工具Log
+        Log.d("taodaren", "onCreate: debug");
+        Log.i(TAG, "onCreate: info");
     }
 }
