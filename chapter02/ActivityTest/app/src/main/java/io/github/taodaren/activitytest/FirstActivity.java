8fdb3a553341f181b20efe79bcffdf107bc37551
taodaren <taodaren1125@gmail.com>
2017-05-25 15:18:32 +0800

    使用 Menu

--- chapter02/ActivityTest/app/src/main/java/io/github/taodaren/activitytest/FirstActivity.java
+++ chapter02/ActivityTest/app/src/main/java/io/github/taodaren/activitytest/FirstActivity.java
@@ -1,7 +1,9 @@
 package io.github.taodaren.activitytest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -19,5 +21,27 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, "按钮被点击！", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //为当前活动创建菜单
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    //定义菜单响应事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "Add 被点击", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "Remove 被点击", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+        }
+        return true;
     }
 }
