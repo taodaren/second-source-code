da81d9f0694ae65aa381b531807142706425608e
taodaren <taodaren1125@gmail.com>
2017-05-25 15:04:23 +0800

    使用Toast

--- chapter02/ActivityTest/app/src/main/java/io/github/taodaren/activitytest/FirstActivity.java
+++ chapter02/ActivityTest/app/src/main/java/io/github/taodaren/activitytest/FirstActivity.java
@@ -2,6 +2,9 @@ package io.github.taodaren.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Button button = (Button) findViewById(R.id.button_1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "按钮被点击！", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
