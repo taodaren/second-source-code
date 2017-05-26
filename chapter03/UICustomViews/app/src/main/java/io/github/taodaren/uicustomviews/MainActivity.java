e1a24f96de3670b3050fefe13b54875f9f882a42
taodaren <taodaren1125@gmail.com>
2017-05-27 06:20:51 +0800

    引入布局并隐藏系统标题栏

--- chapter03/UICustomViews/app/src/main/java/io/github/taodaren/uicustomviews/MainActivity.java
+++ chapter03/UICustomViews/app/src/main/java/io/github/taodaren/uicustomviews/MainActivity.java
@@ -1,5 +1,6 @@
 package io.github.taodaren.uicustomviews;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //隐藏系统自带标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
     }
 }
