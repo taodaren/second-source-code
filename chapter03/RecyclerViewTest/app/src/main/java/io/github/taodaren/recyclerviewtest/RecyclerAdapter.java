2f9eed69a73096b5830b39332976d9f83e9405f6
taodaren <taodaren1125@gmail.com>
2017-05-30 16:47:00 +0800

    RecyclerView 点击事件

--- chapter03/RecyclerViewTest/app/src/main/java/io/github/taodaren/recyclerviewtest/RecyclerAdapter.java
+++ chapter03/RecyclerViewTest/app/src/main/java/io/github/taodaren/recyclerviewtest/RecyclerAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -32,7 +33,24 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
         //加载布局
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_android, parent, false);
         //创建 ViewHolder 实例
-        ViewHolder holder = new ViewHolder(view);
+        final ViewHolder holder = new ViewHolder(view);
+        //设置点击事件
+        holder.rlView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                RecyclerBean bean = beanList.get(position);
+                Toast.makeText(v.getContext(), "布局被点击" + bean.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        holder.imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                RecyclerBean bean = beanList.get(position);
+                Toast.makeText(v.getContext(), "图片被点击" + bean.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
         return holder;
     }
 
@@ -51,6 +69,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
 
     /**
      * 用于告诉 RecyclerView 一共有多少子项
+     *
      * @return 数据源长度
      */
     @Override
@@ -59,6 +78,8 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
+        //保存子项最外层布局的实例
+        View rlView;
         ImageView imageView;
         TextView textView;
 
@@ -67,6 +88,7 @@ public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHo
          */
         public ViewHolder(View itemView) {
             super(itemView);
+            rlView = itemView;
             imageView = (ImageView) itemView.findViewById(R.id.image_android);
             textView = (TextView) itemView.findViewById(R.id.text_android);
         }
