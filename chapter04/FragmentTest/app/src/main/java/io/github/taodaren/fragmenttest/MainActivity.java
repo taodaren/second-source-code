ab55e8ebe122d2a87ee394853934aaa9e235a002
taodaren <taodaren1125@gmail.com>
2017-05-31 10:33:08 +0800

    动态添加 Fragment

--- chapter04/FragmentTest/app/src/main/java/io/github/taodaren/fragmenttest/MainActivity.java
+++ chapter04/FragmentTest/app/src/main/java/io/github/taodaren/fragmenttest/MainActivity.java
@@ -1,7 +1,11 @@
 package io.github.taodaren.fragmenttest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.button:
+                        replaceFragment(new AnotherRightFragment());
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+        replaceFragment(new RightFragment());
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.right_layout, fragment);
+        transaction.commit();
     }
 }
