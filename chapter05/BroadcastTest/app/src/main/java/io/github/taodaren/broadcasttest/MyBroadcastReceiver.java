7139096f75228ed8ff98f4782566f540de4db3a1
taodaren <taodaren1125@gmail.com>
2017-06-01 20:49:06 +0800

    广播截断

--- chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MyBroadcastReceiver.java
+++ chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MyBroadcastReceiver.java
@@ -13,5 +13,7 @@ public class MyBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+        //将本条广播截断
+        abortBroadcast();
     }
 }
