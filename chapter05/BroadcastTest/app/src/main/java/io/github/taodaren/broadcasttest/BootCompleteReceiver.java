49d5d630fa15427c918af820c515a5323f284a47
taodaren <taodaren1125@gmail.com>
2017-06-01 15:56:26 +0800

    静态注册实现开机启动

---
+++ chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/BootCompleteReceiver.java
@@ -0,0 +1,14 @@
+package io.github.taodaren.broadcasttest;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "启动完成", Toast.LENGTH_SHORT).show();
+    }
+}
