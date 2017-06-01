812fa9147baa2d5eb888aeeec8be402e9276afa6
taodaren <taodaren1125@gmail.com>
2017-06-01 12:02:43 +0800

    动态注册监听网络变化

--- chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MainActivity.java
+++ chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MainActivity.java
@@ -1,13 +1,41 @@
 package io.github.taodaren.broadcasttest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private NetWorkChangeReceiver netWorkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        netWorkChangeReceiver = new NetWorkChangeReceiver();
+        //添加 action
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        //注册
+        registerReceiver(netWorkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //取消注册
+        unregisterReceiver(netWorkChangeReceiver);
+    }
+
+    class NetWorkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "网络发生变化", Toast.LENGTH_SHORT).show();
+        }
     }
 }
