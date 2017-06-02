9e89b90d5a11d247a01f2bae4428774e7e5b3a80
taodaren <taodaren1125@gmail.com>
2017-06-02 10:26:15 +0800

    使用本地广播

--- chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MainActivity.java
+++ chapter05/BroadcastTest/app/src/main/java/io/github/taodaren/broadcasttest/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
@@ -14,17 +15,25 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
     private NetWorkChangeReceiver netWorkChangeReceiver;
+    //本地广播
+    private LocalReceiver localReceiver;
+    private LocalBroadcastManager localBroadcastManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+
         intentFilter = new IntentFilter();
-        netWorkChangeReceiver = new NetWorkChangeReceiver();
+//        netWorkChangeReceiver = new NetWorkChangeReceiver();
+        intentFilter.addAction("io.github.taodaren.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
         //添加 action
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+//        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         //注册
-        registerReceiver(netWorkChangeReceiver, intentFilter);
+//        registerReceiver(netWorkChangeReceiver, intentFilter);
+        localBroadcastManager.registerReceiver(localReceiver, intentFilter);
     }
 
     @Override
@@ -32,10 +41,12 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         //取消注册
         unregisterReceiver(netWorkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
     /**
      * 发送自定义广播（标准广播）
+     *
      * @param view
      */
     public void onMyBroadcast(View view) {
@@ -44,6 +55,22 @@ public class MainActivity extends AppCompatActivity {
 //        sendBroadcast(intent);
         //发送有序广播
         sendOrderedBroadcast(intent, null);
+    }
+
+    /**
+     * 发送本地广播
+     */
+    public void onLocalBroadcast(View view) {
+        Intent intent = new Intent("io.github.taodaren.broadcasttest.LOCAL_BROADCAST");
+        localBroadcastManager.sendBroadcast(intent);
+    }
+
+    class LocalReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Toast.makeText(context, "received local broadcast", Toast.LENGTH_SHORT).show();
+        }
     }
 
     class NetWorkChangeReceiver extends BroadcastReceiver {
