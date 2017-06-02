72b8f92b394dfaba10127920d07e400c9178ccef
taodaren <taodaren1125@gmail.com>
2017-06-02 18:08:59 +0800

    WebView 简单使用

--- chapter09/WebViewTest/app/src/main/java/io/github/taodaren/webviewtest/MainActivity.java
+++ chapter09/WebViewTest/app/src/main/java/io/github/taodaren/webviewtest/MainActivity.java
@@ -1,13 +1,20 @@
 package io.github.taodaren.webviewtest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.web_view);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
     }
 }
