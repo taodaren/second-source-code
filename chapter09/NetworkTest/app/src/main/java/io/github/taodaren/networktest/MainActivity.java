de24ef52ecd54ea22f0355fd527f24bc258bf8ce
taodaren <taodaren1125@gmail.com>
2017-06-14 16:32:06 +0800

    Pull 解析

--- chapter09/NetworkTest/app/src/main/java/io/github/taodaren/networktest/MainActivity.java
+++ chapter09/NetworkTest/app/src/main/java/io/github/taodaren/networktest/MainActivity.java
@@ -2,13 +2,18 @@ package io.github.taodaren.networktest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -37,20 +42,66 @@ public class MainActivity extends AppCompatActivity {
         new Thread(new Runnable() {
             @Override
             public void run() {
-                OkHttpClient client = new OkHttpClient();
-                Request request = new Request.Builder()
-                        .url("https://taodaren.github.io/")
-                        .build();
                 try {
+                    OkHttpClient client = new OkHttpClient();
+                    Request request = new Request.Builder()
+//                        .url("https://taodaren.github.io/")
+                            .url("http://localhost:8080/taodaren/get_data.xml")
+                            .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    showResponse(responseData);
+//                    showResponse(responseData);
+                    //解析服务器返回的数据
+                    parseXMLWithPull(responseData);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
             }
         }).start();
+    }
+
+    /**
+     * 解析服务器返回的数据
+     */
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG:
+                        if ("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if ("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if ("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:
+                        if ("app".equals(nodeName)) {
+                            Log.e("taodaren", "parseXMLWithPull: id is " + id);
+                            Log.e("taodaren", "parseXMLWithPull: name is " + name);
+                            Log.e("taodaren", "parseXMLWithPull: version is " + version);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                //获取节点具体内容
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void sendRequestWithHttpURLConnection() {
