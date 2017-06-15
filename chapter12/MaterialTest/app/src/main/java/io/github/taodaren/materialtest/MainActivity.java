14a6a1a6e0f9c33cf0692ada586e04a42a270d66
taodaren <taodaren1125@gmail.com>
2017-06-15 18:44:49 +0800

    滑动菜单 → DrawerLayout

--- chapter12/MaterialTest/app/src/main/java/io/github/taodaren/materialtest/MainActivity.java
+++ chapter12/MaterialTest/app/src/main/java/io/github/taodaren/materialtest/MainActivity.java
@@ -1,6 +1,9 @@
 package io.github.taodaren.materialtest;
 
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -8,13 +11,24 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //设置 ToolBar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //设置滑动菜单
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            //显示 ToolBar 左侧导航按钮
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            //设置（自定义）导航按钮图标
+            actionBar.setHomeAsUpIndicator(android.R.drawable.ic_menu_sort_by_size);
+        }
     }
 
     @Override
@@ -26,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case android.R.id.home://导航按钮固定 id
+                //打开滑动菜单
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
             case R.id.backup:
                 Toast.makeText(this, "You clicked Backup", Toast.LENGTH_SHORT).show();
                 break;
