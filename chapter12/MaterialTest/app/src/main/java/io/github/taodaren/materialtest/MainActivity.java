dd3a66dfb88d79b94a0dc204b0c02eb8fe2cfe08
taodaren <taodaren1125@gmail.com>
2017-06-16 14:13:19 +0800

    使用 Snackbar 提示工具

--- chapter12/MaterialTest/app/src/main/java/io/github/taodaren/materialtest/MainActivity.java
+++ chapter12/MaterialTest/app/src/main/java/io/github/taodaren/materialtest/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -34,8 +35,15 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "FAB 被点击", Toast.LENGTH_SHORT).show();
+            public void onClick(View view) {
+                //使用 Snackbar 提示工具
+                Snackbar.make(view, "数据删除", Snackbar.LENGTH_SHORT)
+                        .setAction("Undo", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Toast.makeText(MainActivity.this, "数据恢复", Toast.LENGTH_SHORT).show();
+                            }
+                        }).show();
             }
         });
     }
