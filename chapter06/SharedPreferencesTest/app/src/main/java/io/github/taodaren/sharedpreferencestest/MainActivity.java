5ad44d251ffa3318e2bab8478475396bf3dd3580
taodaren <taodaren1125@gmail.com>
2017-06-19 19:44:38 +0800

    从 SharedPreferences 中读取数据

--- chapter06/SharedPreferencesTest/app/src/main/java/io/github/taodaren/sharedpreferencestest/MainActivity.java
+++ chapter06/SharedPreferencesTest/app/src/main/java/io/github/taodaren/sharedpreferencestest/MainActivity.java
@@ -1,11 +1,13 @@
 package io.github.taodaren.sharedpreferencestest;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
                 editor.putBoolean("married", false);
                 //将添加的数据提交
                 editor.apply();
+            }
+        });
+
+        findViewById(R.id.btn_restore_data).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //获取 SharedPreferences 对象
+                SharedPreferences sp = getSharedPreferences("sp_data", MODE_PRIVATE);
+                //获取数据（若没找到对应值，则传入默认值）
+                String name = sp.getString("name", "");
+                int age = sp.getInt("age", 0);
+                boolean married = sp.getBoolean("married", false);
+                //打印 log 信息
+                Log.e(TAG, "name is: " + name);
+                Log.e(TAG, "age is: " + age);
+                Log.e(TAG, "married is: " + married);
             }
         });
     }
