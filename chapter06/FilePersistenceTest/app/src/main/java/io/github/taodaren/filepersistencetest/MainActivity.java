89287f0fd37bdb6d6fced6ae6ff37e0e68b19611
taodaren <taodaren1125@gmail.com>
2017-06-19 16:02:18 +0800

    文件存储 → 从文件中读取数据

--- chapter06/FilePersistenceTest/app/src/main/java/io/github/taodaren/filepersistencetest/MainActivity.java
+++ chapter06/FilePersistenceTest/app/src/main/java/io/github/taodaren/filepersistencetest/MainActivity.java
@@ -3,11 +3,17 @@ package io.github.taodaren.filepersistencetest;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +24,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         editText = (EditText) findViewById(R.id.edit_text);
+
+        String inputText = load();
+        //一次性进行两种空值判断
+        if (!TextUtils.isEmpty(inputText)) {
+            //将内容添加到 EditText
+            editText.setText(inputText);
+            //将输入光标移动到文本末尾
+            editText.setSelection(inputText.length());
+            Toast.makeText(this, "还原成功", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * 读取文件中存储的文本内容
+     */
+    private String load() {
+        FileInputStream fis = null;
+        BufferedReader bufferedReader = null;
+        StringBuilder sbContent = new StringBuilder();
+        try {
+            fis = openFileInput("data");
+            bufferedReader = new BufferedReader(new InputStreamReader(fis));
+            String line = "";
+            while ((line = bufferedReader.readLine()) != null) {
+                sbContent.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return sbContent.toString();
     }
 
     /**
