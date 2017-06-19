e9dd14cc8464d2c060aba5bbf29227e142e454e3
taodaren <taodaren1125@gmail.com>
2017-06-19 14:53:44 +0800

    文件存储 → 将数据存储到文件中

--- chapter06/FilePersistenceTest/app/src/main/java/io/github/taodaren/filepersistencetest/MainActivity.java
+++ chapter06/FilePersistenceTest/app/src/main/java/io/github/taodaren/filepersistencetest/MainActivity.java
@@ -1,13 +1,56 @@
 package io.github.taodaren.filepersistencetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = (EditText) findViewById(R.id.edit_text);
+    }
+
+    /**
+     * 活动销毁前一定调用此方法
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        String inputText = editText.getText().toString();
+        save(inputText);
+    }
+
+    /**
+     * 将输入的内容存储到文件中
+     */
+    private void save(String inputText) {
+        FileOutputStream fos = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            //将数据存储到指定文件
+            fos = openFileOutput("data", Context.MODE_PRIVATE);
+            bufferedWriter = new BufferedWriter(new OutputStreamWriter(fos));
+            bufferedWriter.write(inputText);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
