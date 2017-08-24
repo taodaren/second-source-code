9e532738f70b36e34568360f2ed03316d94f30a9
taodaren <taodaren1125@gmail.com>
2017-08-24 15:31:05 +0800

    通知的高级功能

--- chapter08/NotificationTest/app/src/main/java/io/github/taodaren/notificationtest/MainActivity.java
+++ chapter08/NotificationTest/app/src/main/java/io/github/taodaren/notificationtest/MainActivity.java
@@ -5,14 +5,10 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.NotificationCompat;
 import android.view.View;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -35,7 +31,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 //兼容所有版本，设置通知内容
                 Notification notification = new Notification.Builder(this)
                         .setContentTitle("这是通知标题")
-                        .setContentText("通知正文内容")
+                        .setContentText("较长文本在通知中默认显示省略号较长文本在通知中默认显示省略号较长文本在通知中默认显示省略号")
                         //指定创建时间
                         .setWhen(System.currentTimeMillis())
                         //通知小图标
@@ -54,6 +50,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //                        .setLights(Color.BLUE, 1000, 1000)
                         //设置通知默认效果
                         .setDefaults(NotificationCompat.DEFAULT_ALL)
+//                        //在通知中显示长文字
+//                        .setStyle(new NotificationCompat.BigTextStyle().bigText("NotificationCompat.BigTextStyle 对象用于封装长文字"))
+//                        //显示大图片
+//                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher)))
+                        //设置重要程度
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
                         .build();
                 //显示通知
                 manager.notify(1, notification);
