5dab4103df7ffc2dd2c27fff5dfaa238c024237e
taodaren <taodaren1125@gmail.com>
2017-08-25 13:20:42 +0800

    播放音频

--- chapter08/PlayAudioTest/app/src/main/java/io/github/taodaren/playaudiotest/MainActivity.java
+++ chapter08/PlayAudioTest/app/src/main/java/io/github/taodaren/playaudiotest/MainActivity.java
@@ -1,13 +1,111 @@
 package io.github.taodaren.playaudiotest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private MediaPlayer mediaPlayer = new MediaPlayer();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        findViewById(R.id.btn_play).setOnClickListener(this);
+        findViewById(R.id.btn_pause).setOnClickListener(this);
+        findViewById(R.id.btn_stop).setOnClickListener(this);
+
+        //运行时权限处理，动态申请 WRITE_EXTERNAL_STORAGE 权限
+        if (ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            //初始化 MediaPlayer
+            initMediaPlayer();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //如果用户同意权限申请，那么对 MediaPlayer 对象初始化
+                    initMediaPlayer();
+                } else {
+                    //如果用户拒绝了权限申请，那么直接关闭程序
+                    Toast.makeText(this, "拒绝权限将无法使用程序", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    private void initMediaPlayer() {
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "music.mp3");
+            //指定音频文件的路径
+            mediaPlayer.setDataSource(file.getPath());
+            //让 MediaPlayer 进入到准备状态
+            mediaPlayer.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            //释放 MediaPlayer 相关资源
+            mediaPlayer.stop();
+            mediaPlayer.release();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_play:
+                //如果没有正在播放音频
+                if (!mediaPlayer.isPlaying()) {
+                    //开始播放
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.btn_pause:
+                //如果正在播放音频
+                if (mediaPlayer.isPlaying()) {
+                    //暂停播放
+                    mediaPlayer.pause();
+                }
+                break;
+            case R.id.btn_stop:
+                //如果正在播放音频
+                if (mediaPlayer.isPlaying()) {
+                    //停止播放，重置为刚刚创建的状态
+                    mediaPlayer.reset();
+                    initMediaPlayer();
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
